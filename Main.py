32ed25ef55583e7dd908ae57b22e669dd11b1efa
James <jfourie@bu.edu>
2023-11-05 22:11:58 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,9 @@
+#Main 
+
+import numpy as np
+from tensorflow import keras
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('US_Accidents_March23.csv')
