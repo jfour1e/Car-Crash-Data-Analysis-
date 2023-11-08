30430599bd7851051921e8943cc7a6a46a2c5554
James <jfourie@bu.edu>
2023-11-07 22:07:29 -0500

    commit1

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 #Main 
 
 import numpy as np
-from tensorflow import keras
-import tensorflow as tf
+#from tensorflow import keras
+#import tensorflow as tf
 import matplotlib.pyplot as plt
 import pandas as pd
 
 df = pd.read_csv('US_Accidents_March23.csv')
+
