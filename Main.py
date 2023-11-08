b89600bd870e2f796680929a9af674ed0e82455a
James <jfourie@bu.edu>
2023-11-07 22:38:01 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,16 @@
 #Main 
 
+#imports 
 import numpy as np
-#from tensorflow import keras
-#import tensorflow as tf
+from tensorflow import keras
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sb
 
+df = pd.read_csv('US_Accidents_March23.csv')
+#df = pd.read_csv("C:/Users/Shaurya/Desktop/US_Accidents_March23.csv")
 
-#df = pd.read_csv('US_Accidents_March23.csv')
-
-print("Reading CSV file...")
-df = pd.read_csv("C:/Users/Shaurya/Desktop/US_Accidents_March23.csv")
 #print(list(df))
 #dataplot=sb.heatmap(df.corr())
 dfresult = df.dropna()
