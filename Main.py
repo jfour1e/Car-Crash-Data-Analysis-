67ef98cd871cd499225b968a1d9959cadd2056d9
James <jfourie@bu.edu>
2023-11-07 23:22:55 -0500

    column drop

--- Main.py
+++ Main.py
@@ -11,8 +11,10 @@ import seaborn as sb
 df = pd.read_csv('US_Accidents_March23.csv')
 #df = pd.read_csv("C:/Users/Shaurya/Desktop/US_Accidents_March23.csv")
 
-#print(list(df))
 #dataplot=sb.heatmap(df.corr())
-dfresult = df.dropna()
-print(list(dfresult))
-print(list(df))
+#dfresult = df.dropna()
+#print(list(dfresult))
+#print(list(df))
+
+df1 = df.drop(['Source', 'Description', 'Country', 'Timezone', 'Airport_Code', 'Weather_Timestamp', 'Temperature(F)', 'Wind_Direction', 'Wind_Speed(mph)', 'Bump', 'Amenity', 'No_Exit', 'Give_Way', 'Railway', 'Roundabout', 'Station','Traffic_Calming', 'Turning_Loop', 'Civil_Twilight', 'Nautical_Twilight', 'Astronomical_Twilight'],axis='columns')
+print(df1.columns)
